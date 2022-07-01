e1cc68303b8ede7a5c9e23a34fd442822aeb71cf
aasa72 <asa.andersson10@comhem.se>
2022-07-01 16:28:28 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+let bill = 100
+let tip = 10 / 100
+let totalTip = bill * tip
+
+let billInput = document.getElementById("bill").value
+let tipInput = document.getElementById("tip").value
+const totalBill = document.getElementById("totalBill")
+
+ console.log(tipInput)
+
+
+//function totalBill() {
+//  let totalTip = tip / bill
+ 
+
+//console.log(totalTip)
+//}
+//totalBill()
+
+
+
+
+
